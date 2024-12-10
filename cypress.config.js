a9a1f5b658ab8b5e0584b565fb2bccafe1bbbce1
thp <thierrypoupon@hotmail.com>
2024-12-10 16:35:30 +0100

    add test e2e cypress

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    fixturesFolder: 'cypress/fixtures',
+  },
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  video: false,
+  screenshotOnRunFailure: true,
+  defaultCommandTimeout: 10000,
+}) 
